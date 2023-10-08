b0d98272960fbace744681e5a024aac63e932fcc
unknown <nnvedward@gmail.com>
2023-10-08 01:55:11 +0100

    Resume update

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -4,110 +4,110 @@ import Button from "../../shared/button/Button";
 import "./Project.css";
 
 const Project = () => {
-	const featuredProjects = [
-		{
-			title: "Task Manager Api",
-			description: "Rest Api for managing your task",
-			github: "",
-			live: "",
-			stack: "MongoDB + Express + React + Node JS",
-			isPrivate: false,
-		},
-		{
-			title: "Indecision",
-			description:
-				"What do you want to do? Let this application help you decide!",
-			github: "https://github.com/Nnvemeka/Indecision-app",
-			live: "https://nnv-indecision.netlify.app",
-			stack: null,
-			isPrivate: false,
-		},
-		{
-			title: "Chat App",
-			description: "Create a chat room and enjoy your conversation.",
-			github: "https://github.com/Nnvemeka/chat-app",
-			live: "https://nnv-chat.herokuapp.com",
-			stack: "NodeJS + SocketIO + Express",
-			isPrivate: false,
-		},
-		{
-			title: "Weather App",
-			description: "Weather forcast application.",
-			github: "https://github.com/Nnvemeka/weather-app",
-			live: "https://nnv-weather-application.herokuapp.com",
-			stack: "NodeJs + Express",
-			isPrivate: false,
-		},
-	];
-	const projects = [
-		{
-			title: "Prexcribe",
-			description:
-				"Prexcibe is a healthcare Saas platform which aims at strengthening care coordination and informed clinical decision-making in patient management.",
-			github: "",
-			live: "https://app.prexcribe.io",
-			stack: null,
-			isPrivate: true,
-		},
-		{
-			title: "Kellispay",
-			description:
-				"Kellispay is a one-stop-shop payment ecosystem aimed at providing everyday financial access and solutions to it's customers.",
-			github: null,
-			live: "https://kellispay.com",
-			stack: null,
-			isPrivate: true,
-		},
-		{
-			title: "Gezawa Commodity Exchange",
-			description: "GCMX Trading platform.",
-			github: null,
-			live: "http://gcmx.bluetag.tech/auth/market",
-			stack: null,
-			isPrivate: true,
-		},
-		{
-			title: "Butiq",
-			description: "An E-commerce store for Fashion & Style.",
-			github: "https://github.com/Nnvemeka/butiq",
-			live: "https://shop-butiq.vercel.app",
-			stack: null,
-			isPrivate: false,
-		},
-		{
-			title: "Krypto World",
-			description: "A cryptocurrency application.",
-			github: "https://github.com/Nnvemeka/krptoworld",
-			live: "https://kryptoworld.vercel.app",
-			stack: null,
-			isPrivate: false,
-		},
-		{
-			title: "Expensify",
-			description: "Keep track of your expenses!",
-			github: "https://github.com/Nnvemeka/expensify-app",
-			live: "https://nnv-expensify-app.herokuapp.com",
-			stack: null,
-			isPrivate: false,
-		},
-	];
+  const featuredProjects = [
+    {
+      title: "Task Manager Api",
+      description: "Rest Api for managing your task",
+      github: "",
+      live: "",
+      stack: "MongoDB + Express + React + Node JS",
+      isPrivate: false,
+    },
+    {
+      title: "Indecision",
+      description:
+        "What do you want to do? Let this application help you decide!",
+      github: "https://github.com/Nnvemeka/Indecision-app",
+      live: "https://nnv-indecision.netlify.app",
+      stack: null,
+      isPrivate: false,
+    },
+    {
+      title: "Chat App",
+      description: "Create a chat room and enjoy your conversation.",
+      github: "https://github.com/Nnvemeka/chat-app",
+      live: "https://nnv-chat.herokuapp.com",
+      stack: "NodeJS + SocketIO + Express",
+      isPrivate: false,
+    },
+    {
+      title: "Weather App",
+      description: "Weather forcast application.",
+      github: "https://github.com/Nnvemeka/weather-app",
+      live: "https://nnv-weather-application.herokuapp.com",
+      stack: "NodeJs + Express",
+      isPrivate: false,
+    },
+  ];
+  const projects = [
+    {
+      title: "Prexcribe",
+      description:
+        "Prexcibe is a healthcare Saas platform which aims at strengthening care coordination and informed clinical decision-making in patient management.",
+      github: "",
+      live: "https://app.prexcribe.io",
+      stack: null,
+      isPrivate: true,
+    },
+    {
+      title: "Kellispay",
+      description:
+        "Kellispay is a one-stop-shop payment ecosystem aimed at providing everyday financial access and solutions to it's customers.",
+      github: null,
+      live: "https://kellispay.com",
+      stack: null,
+      isPrivate: true,
+    },
+    {
+      title: "Gezawa Commodity Exchange",
+      description: "GCMX Trading platform.",
+      github: null,
+      live: "http://gcmx.bluetag.tech/auth/market",
+      stack: null,
+      isPrivate: true,
+    },
+    {
+      title: "Butiq",
+      description: "An E-commerce store for Fashion & Style.",
+      github: "https://github.com/Nnvemeka/butiq",
+      live: "https://shop-butiq.vercel.app",
+      stack: null,
+      isPrivate: false,
+    },
+    {
+      title: "Krypto World",
+      description: "A cryptocurrency application.",
+      github: "https://github.com/Nnvemeka/krptoworld",
+      live: "https://kryptoworld.vercel.app",
+      stack: null,
+      isPrivate: false,
+    },
+    // {
+    // 	title: "Expensify",
+    // 	description: "Keep track of your expenses!",
+    // 	github: "https://github.com/Nnvemeka/expensify-app",
+    // 	live: "https://nnv-expensify-app.herokuapp.com",
+    // 	stack: null,
+    // 	isPrivate: false,
+    // },
+  ];
 
-	return (
-		<div className="project">
-			<div className="portfolio_showcase">
-				<h3 className="title">PROJECTS</h3>
-				{projects.map((project, index) => (
-					<ProjectCard {...project} key={index} id={index} />
-				))}
-			</div>
-			<div>
-				<Button
-					previous={{ url: "/", text: "Home" }}
-					next={{ url: "/article", text: "Article" }}
-				/>
-			</div>
-		</div>
-	);
+  return (
+    <div className="project">
+      <div className="portfolio_showcase">
+        <h3 className="title">PROJECTS</h3>
+        {projects.map((project, index) => (
+          <ProjectCard {...project} key={index} id={index} />
+        ))}
+      </div>
+      <div>
+        <Button
+          previous={{ url: "/", text: "Home" }}
+          next={{ url: "/article", text: "Article" }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Project;
--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -6,149 +6,164 @@ import "./Resume.css";
 import pdf from "../../static/Edward_Ugwu.pdf";
 
 const Resume = () => {
-	const experiences = [
-		{
-			company: "Prexcribe",
-			position: "Frontend Engineer",
-			started: "Jul 2022",
-			ended: null,
-			description: null,
-			achievements: [
-				"Made recommendations for new technology integration based on suitability and alignment to business goals.",
-				"Increased web application usage by 35% by implementing unit tests.",
-				"Liaised between marketing and IT on web development projects integrating front-end assets to back-end systems.",
-				"Teamed on user-centric design strategy in translation of UX and business requirements into coded solutions.",
-			],
-		},
-		{
-			company: "BlueTag Technologies Limited",
-			position: "Frontend Engineer",
-			started: "Jan 2020",
-			ended: "Nov 2022",
-			description: null,
-			achievements: [
-				"Updated old code bases to modern development standards, improving functionality by 20%.",
-				"Collaborated with project managers to select ambitious, but realistic coding milestones on pre-release software project development.",
-				"Integrated constructive programs into cohesive product.",
-				"Designed intuitive graphical user interfaces to improve user experience.",
-				"Integrated software components into fully functional software system.",
-			],
-		},
-		{
-			company: "Kellispay",
-			position: "Frontend Developer (Contract)",
-			started: "Jan 2022",
-			ended: "Jun 2022",
-			description: null,
-			achievements: [
-				"Planned and developed interfaces that simplified overall management and ease usage.",
-				"Optimized website and increased speed by 15%.",
-				"Built and deployed scalable mobile responsive User Interfaces for maximized User experience.",
-				"Contributed ideas and suggestions in team meetings and delivered updates on deadlines, designs and enhancements",
-			],
-		},
-		{
-			company: "Davayte",
-			position: "Frontend Engineer (Contract)",
-			started: "Jan 2019",
-			ended: "Dec 2019",
-			description: null,
-			achievements: [
-				"Worked cooperatively with client services, sales and design team in deadline-driven environment.",
-				"Designed and updated layouts to meet usability and performance requirements, increasing app speed by 10%.",
-				"Coded using HTML, CSS and JavaScript to develop features for both mobile and desktop platforms.",
-				"Improved tools to boost user interaction and deliver design versatility.",
-			],
-		},
-		{
-			company: "Nigerian Breweries PLC",
-			position: "Software Developer (IT)",
-			started: "Apr 2018",
-			ended: "Sept 2018",
-			description: null,
-			achievements: [
-				"Built and deployed scalable mobile responsive User Interfaces for maximized User experience.",
-				"Assisted with search engine optimization (SEO) strategies and techniques.",
-				"Worked with version control systems such as Git and SVN.",
-				"Wrote well designed, testable and efficient code.",
-				"Participated in code reviews to ensure best practices.",
-			],
-		},
-	];
+  const experiences = [
+    {
+      company: "Freexit Technologies",
+      position: "Frontend Engineer(Part Time)",
+      started: "Feb 2023",
+      ended: null,
+      description: null,
+      achievements: [
+        "Spearheaded the frontend development team in architecting and implementing a cutting-edge logistics software platform.",
+        "Leveraged Next.js to create a highly responsive and user-friendly interface for our logistics service, ensuring an exceptional user experience.",
+        "Implemented robust security measures to safeguard customer data and financial transactions, ensuring the safety and trust of our users.",
+        "Led efforts to enhance the platform's scalability, performance, and reliability to meet the demands of a growing user base.",
+        "Fostered a culture of innovation, encouraging team members to explore and implement new technologies and features that enhance the service.",
+      ],
+    },
+    {
+      company: "Prexcribe",
+      position: "Lead Frontend Engineer(Part Time)",
+      started: "Jul 2022",
+      ended: null,
+      description: null,
+      achievements: [
+        "Made recommendations for new technology integration based on suitability and alignment to business goals.",
+        "Designed and optimized responsive user interfaces, enabling seamless access to critical patient data and clinical decision support on various devices.",
+        "Collaborated with backend developers to establish efficient data communication and integration, enabling real-time access to patient records and clinical insights.",
+        "Teamed on user-centric design strategy in translation of UX and business requirements into coded solutions.",
+        "Implemented stringent security measures to ensure compliance with healthcare data regulations, safeguarding patient information and privacy.",
+      ],
+    },
+    {
+      company: "BlueTag Technologies Limited",
+      position: "Software Developer(Full Time)",
+      started: "Jan 2020",
+      ended: "Nov 2022",
+      description: null,
+      achievements: [
+        "Updated old code bases to modern development standards, improving functionality by 20%.",
+        "Collaborated with project managers to select ambitious, but realistic coding milestones on pre-release software project development.",
+        "Integrated constructive programs into cohesive product.",
+        "Designed intuitive graphical user interfaces to improve user experience.",
+        "Integrated software components into fully functional software system.",
+      ],
+    },
+    {
+      company: "Kellispay",
+      position: "Frontend Developer (Contract)",
+      started: "Jan 2022",
+      ended: "Jun 2022",
+      description: null,
+      achievements: [
+        "Planned and developed interfaces that simplified overall management and ease usage.",
+        "Optimized website and increased speed by 15%.",
+        "Built and deployed scalable mobile responsive User Interfaces for maximized User experience.",
+        "Contributed ideas and suggestions in team meetings and delivered updates on deadlines, designs and enhancements",
+      ],
+    },
+    {
+      company: "Davayte",
+      position: "Frontend Engineer (Contract)",
+      started: "Jan 2019",
+      ended: "Dec 2019",
+      description: null,
+      achievements: [
+        "Worked cooperatively with client services, sales and design team in deadline-driven environment.",
+        "Designed and updated layouts to meet usability and performance requirements, increasing app speed by 10%.",
+        "Coded using HTML, CSS and JavaScript to develop features for both mobile and desktop platforms.",
+        "Improved tools to boost user interaction and deliver design versatility.",
+      ],
+    },
+    {
+      company: "Nigerian Breweries PLC",
+      position: "Software Developer (IT)",
+      started: "Apr 2018",
+      ended: "Sept 2018",
+      description: null,
+      achievements: [
+        "Built and deployed scalable mobile responsive User Interfaces for maximized User experience.",
+        "Assisted with search engine optimization (SEO) strategies and techniques.",
+        "Worked with version control systems such as Git and SVN.",
+        "Wrote well designed, testable and efficient code.",
+        "Participated in code reviews to ensure best practices.",
+      ],
+    },
+  ];
 
-	return (
-		<div className="resume">
-			<div className="resume_container">
-				<div className="top_row">
-					<h1>Edward Ugwu</h1>
-					<div className="buttons">
-						<a href="https://www.linkedin.com/in/nnvemeka/">
-							<small>
-								<i className="fab fa-linkedin"></i> <span>LinkedIn</span>
-							</small>
-						</a>
-						<a href={pdf} download>
-							<small>
-								<i className="fas fa-download"></i> <span>Download </span>
-							</small>
-						</a>
-					</div>
-				</div>
-				<p>Software Engineer</p>
-				<div className="summary">
-					<p>
-						An experienced front-end engineer with demonstrated expertise in
-						React, Angular, HTML, CSS, JavaScript and Typescript, I specialize
-						in developing responsive websites and web applications that provide
-						a beautiful, intuitive user experience and drive maximum engagement
-						and conversions. I have a keen eye for detail and have successfully
-						applied my skills to the financial, e-commerce, and health care
-						industries. With a passion for creating top-notch user experiences,
-						I take pride in my work and always strive to create the best user
-						experience possible.
-					</p>
-				</div>
-				<div className="divider">
-					<p>Tools & Technologies</p>
-					<span></span>
-				</div>
-				<div className="skills">
-					<p>
-						React, Angular, JavaScript, Typescript, HTML/CSS/SASS/SCSS,
-						Bootstrap, Chakra UI, MongoDB, MySQL, Git, Node.JS, Express,
-						Firebase, REST API, C#, ASP.Net
-					</p>
-				</div>
-				<div className="divider">
-					<p>Experiences</p>
-					<span></span>
-				</div>
-				<div>
-					{experiences.map((experience, index) => (
-						<ResumeCard {...experience} key={index} id={index} />
-					))}
-				</div>
-				<div className="divider">
-					<p>Projects</p>
-					<span></span>
-				</div>
-				<div className="exp_container">
-					<section className="experience">
-						<article className="description">
-							<p className="nospan">
-								Links to my works can be found on{" "}
-								<Link to="/project"> Projects</Link>
-							</p>
-						</article>
-					</section>
-				</div>
-			</div>
-			<Button
-				previous={{ url: "/article", text: "Article" }}
-				next={{ url: "/", text: "Home" }}
-			/>
-		</div>
-	);
+  return (
+    <div className="resume">
+      <div className="resume_container">
+        <div className="top_row">
+          <h1>Edward Ugwu</h1>
+          <div className="buttons">
+            <a href="https://www.linkedin.com/in/nnvemeka/">
+              <small>
+                <i className="fab fa-linkedin"></i> <span>LinkedIn</span>
+              </small>
+            </a>
+            <a href={pdf} download>
+              <small>
+                <i className="fas fa-download"></i> <span>Download </span>
+              </small>
+            </a>
+          </div>
+        </div>
+        <p>Software Engineer</p>
+        <div className="summary">
+          <p>
+            An experienced front-end engineer with demonstrated expertise in
+            React, Next, Angular, JavaScript and Typescript, I specialize in
+            developing responsive websites and web applications that provide a
+            beautiful, intuitive user experience and drive maximum engagement
+            and conversions. I have a keen eye for detail and have successfully
+            applied my skills to the financial, e-commerce, and health care
+            industries. With a passion for creating top-notch user experiences,
+            I take immense pride in my work, continually pursuing excellence
+            while closely aligning with product objectives.
+          </p>
+        </div>
+        <div className="divider">
+          <p>Tools & Technologies</p>
+          <span></span>
+        </div>
+        <div className="skills">
+          <p>
+            React, Next, Angular, JavaScript, Typescript, HTML5/CSS3/SASS/SCSS,
+            Bootstrap, Chakra UI, MongoDB, MySQL, Git, Node.JS, Express,
+            Firebase, JSON, C#,
+          </p>
+        </div>
+        <div className="divider">
+          <p>Experiences</p>
+          <span></span>
+        </div>
+        <div>
+          {experiences.map((experience, index) => (
+            <ResumeCard {...experience} key={index} id={index} />
+          ))}
+        </div>
+        <div className="divider">
+          <p>Projects</p>
+          <span></span>
+        </div>
+        <div className="exp_container">
+          <section className="experience">
+            <article className="description">
+              <p className="nospan">
+                Links to my works can be found on{" "}
+                <Link to="/project"> Projects</Link>
+              </p>
+            </article>
+          </section>
+        </div>
+      </div>
+      <Button
+        previous={{ url: "/article", text: "Article" }}
+        next={{ url: "/", text: "Home" }}
+      />
+    </div>
+  );
 };
 
 export default Resume;
